2a0296624394ed40bc9caa8478f9a25f163998d4
WillWootton03 <willwootton03@gmail.com>
2026-07-29 11:40:41 -0700

    fix : Added unit tests for new calcs, fixed integration testing for updating body details

--- shared/tests/body_calcs.unit.test.ts
+++ shared/tests/body_calcs.unit.test.ts
@@ -1,16 +1,32 @@
 import {describe, expect, it} from '@jest/globals';
-import { getBMR, getTDEE } from '../functions/body_calcs';
+import { getBMR, getCalorieChange, getTDEE } from '../functions/body_calcs';
 
 describe('Valid inputs for body calcs', () =>{
-    it('Should return 1700 < x < 1750', () =>{
+    it('getBMR should return 1700 < x < 1750', () =>{
         const res = getBMR(180, 72, 25, 'male');
         expect(res).toBeGreaterThan(1700);
         expect(res).toBeLessThan(1750);
     });
-    it('Should return 2650 < x < 2700', () => {
+    it('getTDEE should be 2650 < x < 2700', () => {
         const res = getTDEE(getBMR(180, 72, 25, 'male'), 3);
 
         expect(res).toBeGreaterThan(2650);
         expect(res).toBeLessThan(2700);
+    });
+    it('getCalorieChange should return correct value for losing weight over 12 months imperial measurement', async() => {
+        expect(getCalorieChange(200, 180, 360, 'imperal')).toBeLessThan(-192);
+        expect(getCalorieChange(180, 200, 360, 'imperial')).toBeGreaterThan(-195);
+    });
+    it('getCalorieChange should return 192 for gaining weight over 12 months imperial measurement', async() => {
+        expect(getCalorieChange(180, 200, 360, 'imperial')).toBeGreaterThan(190);
+        expect(getCalorieChange(180, 200, 360, 'imperial')).toBeLessThan(195);
+    });
+        it('getCalorieChange should return correct value for losing weight over 12 months metric measurement', async() => {
+        expect(getCalorieChange(90.72, 81.65, 360, 'imperal')).toBeLessThan(-192);
+        expect(getCalorieChange(90.72, 81.65, 360, 'imperial')).toBeGreaterThan(-195);
+    });
+    it('getCalorieChange should return 192 for gaining weight over 12 months metric measurement', async() => {
+        expect(getCalorieChange(81.65, 90.72, 360, 'metric')).toBeGreaterThan(190);
+        expect(getCalorieChange(81.65, 90.72, 360, 'metric')).toBeLessThan(195);
     });
 });
